852a7060bf42f6535e7b9dbc92c58eb73e1c13fe
DaraDjango12 <supertlux@gmail.com>
2023-11-09 13:49:06 +0000

    ourfriend

--- src/Pages/OurFriendsExpanded/Ourfriends.js
+++ src/Pages/OurFriendsExpanded/Ourfriends.js
@@ -2,7 +2,7 @@ import React from 'react'
 import OuterLayout from '../../Layouts/Index'
 import Toppage from '../../Components/Toppage'
 import image from '../../asset/image3.jpg'
-import { Box, Center, Flex, Image } from '@chakra-ui/react'
+import { Box, Center, Flex, Image, Text } from '@chakra-ui/react'
 import { useSelector } from 'react-redux'
 
 const Ourfriends = () => {
@@ -14,17 +14,26 @@ const Ourfriends = () => {
 
   return (
    <OuterLayout>
-   <Toppage image={image} text={'Name'}/>
+   <Toppage image={image} text={friendData.Name}/>
    <Box mt={'8rem'} px={'15%'}>
-        <Flex gap={4} wrap={'wrap'} alignItems={'center'} justifyContent={'center'}>
+        <Flex flexDirection={{base:'column',lg:"row"}} gap={4} p={'2rem'} bg={"gray.200"} alignItems={'center'} justifyContent={'center'}>
          
+          <Box w={'50%'} m={'auto'} >
           <Box w={'18rem'} h={'18rem'} >
-            <Image h={'100%'} w={'100%'} src={"item.photo"}/>
+            <Image h={'100%'} w={'100%'} src={friendData.photo}/>
+          </Box>
+          </Box>
+          <Box>
+          <p color='orange' fontSize={'2rem'} >{friendData.Name}</p>
+          <Flex>
+            <p>Position:{friendData.category}</p>
+          </Flex>
+            <Text  >{friendData.story}</Text>
           </Box>
         
 
         </Flex>
-        <Center h={'10rem'} fontSize={'3rem'} mt={8} bgColor={'grey'} fontWeight={'500'}>Moments</Center>
+        
       </Box>
 
    </OuterLayout>
