0140ee0eeaa17957068f070d2cb2e6e7d18dec20
DaraDjango12 <supertlux@gmail.com>
2023-12-03 21:28:01 +0000

    proposal video

--- src/Pages/capturedMoments/Moments.js
+++ src/Pages/capturedMoments/Moments.js
@@ -1,29 +1,39 @@
 import React from "react";
 import OuterLayout from "../../Layouts/Index";
-import { Box, Center, Flex, Image, Text } from "@chakra-ui/react";
-import Button from "../../Components/Button";
-import { HiOutlineArrowNarrowDown } from "react-icons/hi";
+import { Box, Center, Flex, Image} from "@chakra-ui/react";
+// import Button from "../../Components/Button";
+// import { HiOutlineArrowNarrowDown } from "react-icons/hi";
 import image1 from "../../asset/image1.jpg";
+// import image11 from "../../asset/moments/1.jpeg";
 import Toppage from "../../Components/Toppage";
+import { MomentPictures } from "../../Utils/capturedUtils/momentsPictures";
 
 const Moments = () => {
-
-  const MomentPictures=[{photo:"https://bit.ly/code-beast"},{photo:"https://bit.ly/code-beast"},{photo:"https://bit.ly/code-beast"},{photo:"https://bit.ly/code-beast"},{photo:"https://bit.ly/code-beast"},{photo:"https://bit.ly/code-beast"},{photo:"https://bit.ly/code-beast"},{photo:"https://bit.ly/code-beast"}]
-
-
   return (
     <OuterLayout>
-      <Toppage text={'Moments'} image={image1} />
-      <Box mt={'8rem'} px={'15%'}>
-        <Flex gap={4} wrap={'wrap'} alignItems={'center'} justifyContent={'center'}>
-        { MomentPictures.map((item,Index)=>(
-          <Box w={'18rem'} h={'18rem'} >
-            <Image h={'100%'} w={'100%'} src={item.photo}/>
-          </Box>
-        ))}
-
+      <Toppage text={"Moments"} image={image1} />
+      <Box mt={"8rem"} px={"15%"}>
+        <Flex
+          gap={4}
+          wrap={"wrap"}
+          alignItems={"center"}
+          justifyContent={"center"}
+        >
+          {MomentPictures.map((item, Index) => (
+            <Box w={"18rem"} h={'auto'}>
+              <Image w={"100%"} h={"auto"} src={item.photo} />
+            </Box>
+          ))}
         </Flex>
-        <Center h={'10rem'} fontSize={'3rem'} mt={8} bgColor={'grey'} fontWeight={'500'}>Moments</Center>
+        <Center
+          h={"10rem"}
+          fontSize={"3rem"}
+          mt={8}
+          bgColor={"grey"}
+          fontWeight={"500"}
+        >
+          Moments
+        </Center>
       </Box>
     </OuterLayout>
   );
