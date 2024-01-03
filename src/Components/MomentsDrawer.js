3d118e81438802775baf3e9a8d0ffb9d89697c1d
DaraDjango12 <supertlux@gmail.com>
2024-01-03 09:35:51 +0000

    oloba

--- src/Components/MomentsDrawer.js
+++ src/Components/MomentsDrawer.js
@@ -4,40 +4,48 @@ import React, { useState } from "react";
 import picture from "../asset/image3.jpg";
 import { useNavigate } from "react-router-dom";
 import Moments from "../Pages/capturedMoments/Moments";
-import tele13 from "../asset/moments/telegram/tele13.jpeg"
+import tele13 from "../asset/moments/telegram/tele13.jpeg";
 // import MomentsProposalPreWedding from "./MomentsProposalPreWedding";
-
 
 function SlideFadeEx() {
   const [isHovered1, setIsHovered1] = useState(false);
   const [isHovered2, setIsHovered2] = useState(false);
   const [isHovered3, setIsHovered3] = useState(false);
 
-  const nav= useNavigate()
-
+  const nav = useNavigate();
 
   return (
-
-    <Flex w={"100%"} gap={2} mt={12} wrap={"wrap"} justifyContent={"space-between"}>
+    <Flex
+      w={"100%"}
+      gap={2}
+      mt={12}
+      wrap={"wrap"}
+      justifyContent={"space-between"}
+    >
       <Flex
         flexDirection={"column"}
         justify={"space-between"}
-        w={["100%","100%","32%","32%","32%"]}
+        w={["100%", "100%", "32%", "32%", "32%"]}
         h="30rem"
-        // bgColor="blue.200"
+        bgColor={"red"}
         m="2"
         cursor="pointer"
         onMouseEnter={() => setIsHovered1(true)}
         onMouseLeave={() => setIsHovered1(false)}
         bgImage={tele13}
-        bgSize={"cover"}
+        bgSize={"contain"}
         bgPosition="center"
-        boxShadow={'13px 17px 15px -3px rgba(0,0,0,0.1)'}
+        boxShadow={"13px 17px 15px -3px rgba(0,0,0,0.1)"}
       >
         <Box>.</Box>
 
         <SlideFade in={isHovered1}>
-        <Box py="20" bgColor="rgba(63,58,50,0.6)" onClick={() => nav("/moments")} color="white">
+          <Box
+            py="20"
+            bgColor="rgba(63,58,50,0.6)"
+            onClick={() => nav("/moments")}
+            color="white"
+          >
             <Center fontSize={"2rem"} _hover={{ color: "orange.orange500" }}>
               Moments
             </Center>
@@ -48,7 +56,7 @@ function SlideFadeEx() {
       <Flex
         flexDirection={"column"}
         justify={"space-between"}
-        w={["100%","100%","32%","32%","32%"]}
+        w={["100%", "100%", "32%", "32%", "32%"]}
         h="30rem"
         // bgColor="blue.200"
 
@@ -59,11 +67,16 @@ function SlideFadeEx() {
         bgImage={picture}
         bgSize={"cover"}
         bgPosition="center"
-        boxShadow={'13px 17px 15px -3px rgba(0,0,0,0.1)'}
+        boxShadow={"13px 17px 15px -3px rgba(0,0,0,0.1)"}
       >
         <Box>.</Box>
         <SlideFade in={isHovered2}>
-          <Box py="20" bgColor="rgba(63,58,50,0.6)" onClick={() => nav("/proposal")} color="white">
+          <Box
+            py="20"
+            bgColor="rgba(63,58,50,0.6)"
+            onClick={() => nav("/proposal")}
+            color="white"
+          >
             <Center fontSize={"2rem"} _hover={{ color: "orange.orange500" }}>
               Proposal
             </Center>
@@ -73,7 +86,7 @@ function SlideFadeEx() {
       <Flex
         flexDirection={"column"}
         justify={"space-between"}
-        w={["100%","100%","32%","32%","32%"]}
+        w={["100%", "100%", "32%", "32%", "32%"]}
         h="30rem"
         // bgColor="red.200"
 
@@ -81,20 +94,24 @@ function SlideFadeEx() {
         cursor="pointer"
         onMouseEnter={() => setIsHovered3(true)}
         onMouseLeave={() => setIsHovered3(false)}
-        bgImage='https://bit.ly/kent-c-dodds'
+        bgImage="https://bit.ly/kent-c-dodds"
         bgSize={"cover"}
         bgPosition="center"
-        boxShadow={'13px 17px 15px -3px rgba(0,0,0,0.1)'}
+        boxShadow={"13px 17px 15px -3px rgba(0,0,0,0.1)"}
       >
         <Box>.</Box>
         <SlideFade in={isHovered3}>
-          <Box py="20" bgColor="rgba(63,58,50,0.6)" onClick={() => nav("/preWedding")} color="white">
+          <Box
+            py="20"
+            bgColor="rgba(63,58,50,0.6)"
+            onClick={() => nav("/preWedding")}
+            color="white"
+          >
             <Center
               px={"auto"}
               fontSize={"2rem"}
               _hover={{ color: "orange.orange500" }}
             >
-              
               Pre-Wedding Shoots
             </Center>
           </Box>
