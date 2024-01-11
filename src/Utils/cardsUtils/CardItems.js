09fd47476621d3e77e12264df9eab267cd895164
Adeleke Moyinoluwa Solomon <moyinadeleke@yahoo.com>
2024-01-11 10:28:35 +0100

    update

--- src/Utils/cardsUtils/CardItems.js
+++ src/Utils/cardsUtils/CardItems.js
@@ -93,13 +93,12 @@ export const cardItems = [
   {
     Name: "Badejo Mosopefoluwa",
     photo: ope,
-    summary: "epele sir ",
+    summary: "What I know about the couple. I know ireti is... ",
     category: "BRIDESMAID",
-    story: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-     ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-      in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-       non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`,
+    story: `What I know about the couple.
+
+    I know ireti is strong willed and dedicated then Solomon is a determined and Kind person 
+    The 2 of them will make an amazing home and I wish them all the benefit attached to marriage.`,
   },
   {
     Name: "Ogungbire Gabriel",
